1dbced458d260a49c4ac11e4306ad6f60359b436
celishere <celispost@icloud.com>
2024-07-06 02:21:00 +0300

    Новые сторы

--- frontend/src/providers/StoreProvider/config/StateSchema.ts
+++ frontend/src/providers/StoreProvider/config/StateSchema.ts
@@ -1,5 +1,9 @@
 import { VacanciesStoreSchema } from "../../../entities/Vacancy/model/types/vacanciesStoreSchema.ts";
+import { ResumesStoreSchema } from "../../../entities/Resume/model/types/resumesStoreSchema.ts";
+import { CategoryStoreSchema } from "../../../entities/Category/model/types/categoryStoreSchema.ts";
 
 export interface StateSchema {
     vacancies: VacanciesStoreSchema;
+    resumes: ResumesStoreSchema;
+    category: CategoryStoreSchema;
 }
--- frontend/src/providers/StoreProvider/config/store.ts
+++ frontend/src/providers/StoreProvider/config/store.ts
@@ -3,11 +3,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { StateSchema } from "./StateSchema";
 
 import { vacanciesStoreReducer } from "../../../entities/Vacancy/model/slices/vacanciesStore.ts";
+import { resumesStoreReducer } from "../../../entities/Resume/model/slices/resumesStore.ts";
+import { categoryStoreReducer } from "../../../entities/Category/model/slices/categoryStore.ts";
 
 export function createReduxStore(initialState?: StateSchema) {
   return configureStore({
     reducer: {
       vacancies: vacanciesStoreReducer,
+      resumes: resumesStoreReducer,
+      category: categoryStoreReducer
     },
     devTools: process.env.NODE_ENV === "development",
     preloadedState: initialState,
